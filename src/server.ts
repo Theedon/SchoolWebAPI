37791c924e1e9886351cdd7d64db43ca7c8ae5ec
Theedon <faniyantoyin@gmail.com>
2024-03-05 14:06:23 +0100

    feat: add routes for teachers to app

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
 import express, { type Express } from "express";
 import "dotenv/config";
+import router from "./routes/Teacher";
 
 const app: Express = express();
 
-const PORT = process.env.PORT ?? 3000;
-
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log(req.method);
-  res.send("Hello World");
-});
+app.use("/teachers", router);
 
+// Start Server
+const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
