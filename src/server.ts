e991f8d66b8be864fc08bd0f26936b1c63e73025
Theedon <faniyantoyin@gmail.com>
2024-03-05 20:50:35 +0100

    feat: add middleware to catch unhandled routes

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,12 @@ import classroomRouter from "./routes/Classroom";
 
 const app: Express = express();
 
+// Home URL is introductory URL
+app.get("/", (req, res) => {
+  res.send("<h1>School Web API</h1>");
+});
+
+// Parses incoming requests with JSON payloads.
 app.use(express.json());
 
 app.use("/teachers", teacherRouter);
@@ -13,6 +19,23 @@ app.use("/teachers", teacherRouter);
 app.use("/students", studentRouter);
 
 app.use("/classrooms", classroomRouter);
+
+// Middleware to catch unhandled routes
+app.use((_req, _res, next) => {
+  const error = new Error("ROUTE NOT FOUND");
+  // pass the errorr to the next middleware
+  next(error);
+});
+
+// Error handling middleware
+app.use((err, _req, res, _next) => {
+  res.status(err.status ?? 500);
+  res.send({
+    error: {
+      message: err.message ?? "Internal Server Error",
+    },
+  });
+});
 
 // Start Server
 const PORT = process.env.PORT ?? 3000;
