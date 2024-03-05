655442e2958044332e6787f83125793daef0246e
Theedon <faniyantoyin@gmail.com>
2024-03-05 16:57:13 +0100

    feat(classroom): add routes for classrooms

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { type Express } from "express";
 import "dotenv/config";
 import teacherRouter from "./routes/Teacher";
 import studentRouter from "./routes/Student";
+import classroomRouter from "./routes/Classroom";
 
 const app: Express = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use("/teachers", teacherRouter);
 
 app.use("/students", studentRouter);
+
+app.use("/classrooms", classroomRouter);
 
 // Start Server
 const PORT = process.env.PORT ?? 3000;
